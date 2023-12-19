4e74cb2b5dc4491c5dc99e57681651d4534c2620
oBielov <obielov@telecard.kiev.ua>
2023-12-19 20:03:24 +0200

    init

---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <Head>
+                <title>About Us</title>
+                <meta name="description" content="Learn more about us" />
+            </Head>
+
+            <main>
+                <h1>About Us</h1>
+                <p>Welcome to the about page!.</p>
+            </main>
+        </div>
+    )
+}
+
+export default About
